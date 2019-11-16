f1bfb5727a312a21260ac356671e907ff0b8fb6f
kartikeya01 <kartikeyasharma04@gmail.com>
2019-11-16 00:31:03 -0500

    Added firebase authentication support

---
+++ config/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBIbNKbPgEhAiJvWJTSn_scgxSyfqWGkG4",
+    authDomain: "foodbuff-9f6a1.firebaseapp.com",
+    databaseURL: "https://foodbuff-9f6a1.firebaseio.com",
+    projectId: "foodbuff-9f6a1",
+    storageBucket: "foodbuff-9f6a1.appspot.com",
+    messagingSenderId: "538905388278",
+    appId: "1:538905388278:web:07488319ef4a5652ca38d7"
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
